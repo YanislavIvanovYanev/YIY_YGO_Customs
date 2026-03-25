baa9d0e344ad319679be5f14785555b213907a37
YIYanev20 <157513623+YanislavIvanovYanev@users.noreply.github.com>
2026-03-25 23:16:44 +0100

    Add Spiritual and Evolution

--- Scripts/DB Custom Frames.js
+++ Scripts/DB Custom Frames.js
@@ -14,12 +14,28 @@
     const LINK_FUSION_NAMES = ["Avendread Savior", "a"]
     const LINK_FUSION_FRAME = "https://yanislavivanovyanev.github.io/YIY_YGO_Customs/Frames/Custom/LinkFusion.png";
 
+    const EVOLUTION_NAMES = ["Avendread Savior", "Horus the Black Flame Dragon LV8", "Horus the Black Flame Deity", "Ruddy Rose Dragon",
+                             "Sphere of Chaos", "Elemental HERO Spirit of Neos", "Light and Darkness Dragonlord", "Cyberdark End Dragon"]
+                             //"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+    const EVOLUTION_FRAME = "https://yanislavivanovyanev.github.io/YIY_YGO_Customs/Frames/Custom/Evolution.png";
+
+    const SPIRITUAL_NAMES = ["Jack-o-Bolan", "Doomkaiser Dragon / Assault Mode", "Shell of Chaos"]
+                             //"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+    const SPIRITUAL_FRAME = "https://yanislavivanovyanev.github.io/YIY_YGO_Customs/Frames/Custom/Spiritual.png";
+
     function applyCustomFrame(cardFront, cardName) {
         if (!cardFront || !cardName) return;
 
         if (LINK_FUSION_NAMES.includes(cardName)) {
             cardFront.find(".card_color").attr("src", LINK_FUSION_FRAME);
         }
+        else if (EVOLUTION_NAMES.includes(cardName)) {
+            cardFront.find(".card_color").attr("src", EVOLUTION_FRAME);
+        }
+        else if (SPIRITUAL_NAMES.includes(cardName)) {
+            cardFront.find(".card_color").attr("src", SPIRITUAL_FRAME);
+        }
+
     }
 
 //     /**
